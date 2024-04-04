91b27721a90cedf5daa0d93311f8a34a3f4af2ac
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-04-04 13:56:16 -0700

    Add flickr gem to gemfile and add image url generation to index view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
   def index
+    flickr = Flickr.new "9003e2847c545cfb55f070ba8ba64e09", "0f5c9b74f9b2d307"
+    @photos = flickr.photos.search(user_id: params[:user_id])
   end
 end
